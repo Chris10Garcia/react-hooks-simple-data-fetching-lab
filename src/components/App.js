b3e0c3f17512563ed7a9893d218387c5701b94d5
Christian Garcia <bones192nd@gmail.com>
2023-04-04 13:29:31 -0400

    lab submission

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
-// create your App component here
+import React, { useEffect, useState } from 'react'
+
+
+
+function App (){
+    const [image, setImage] = useState(null)
+
+    function handleImage(data){
+        setImage(data)
+    }
+
+    useEffect( () => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(res => res.json())
+        .then(data => handleImage(data.message))
+    }, [])
+
+
+    return (
+        image ? <img src = {image} alt = "A Random Dog"></img> : <p>Loading...</p>
+    )
+}
+
+
+export default App
